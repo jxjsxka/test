5b972affb1fc86f138f49a08e75e2af1fc5c6a7d
zymm <40225812+jxjsxka@users.noreply.github.com>
2022-10-10 20:36:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
+  return weather['weather'], str(math.floor(weather['temp'])) + "°", math.floor(weather['low']), math.floor(weather['high'])
 
 # 明日天气
 def get_tomorrow_weather():
